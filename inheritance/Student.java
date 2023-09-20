097a0c6fdd82dc2ae1d3f3a60059a6c912b87f9b
Akbar Sami <109865799+akbarsami21@users.noreply.github.com>
2023-09-20 19:44:31 +0600

    Update Student.java

--- inheritance/Student.java
+++ inheritance/Student.java
@@ -4,8 +4,8 @@ public class Student extends Person {
 	public static void main(String[] args) {
 
    Student s1=new Student();
-   s1.getStudentInfo("Md Ali Akbar Sami", "2104010202253", "Md.Azam Khan", "Shanaz Begum",
-		             "mdsami6251@gamil.com", "01720423399", "O+", "Hathazari,Chittagong");
+   s1.getStudentInfo("X", "210", "Y", "Z",
+		             "xy@gamil.com", "01234567891", "O+", "Hathazari,Chittagong");
    s1.printStudentInfo();
 }
 }
